6e108bdc344394cd11a827af08d8e83ebe1a13cc
emir paradi <emir.alejandro.paradi@gmail.com>
2024-01-26 13:37:40 -0300

    Ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Container } from "react-bootstrap"
 import FormularioColores from "./components/FormularioColores"
-import ContenedorCards from "./components/ContenedorCards"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
 
 
 function App() {
@@ -8,9 +11,11 @@ function App() {
 
   return (
     <>
-      <Container className="border border-primary rounded-5 mt-5 mb-5" >
+    <Navbar></Navbar>
+      <Container className="border border-primary rounded-5 mt-5 mb-5 main" >
         <FormularioColores></FormularioColores>
       </Container>
+      <Footer></Footer>
     </>
   )
 }
